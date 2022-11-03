9b791186808d3b6a29e5f6dd4520c28f86cb64c7
Roman Lazuko <lazuko.romann@gmail.com>
2022-11-03 12:07:33 +0300

    new commit

--- app/src/main/java/com/romanlazuko/deleteapp/MainActivity.java
+++ app/src/main/java/com/romanlazuko/deleteapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.print("Activity was created");
+        System.out.print("Activity was developed");
     }
 }
